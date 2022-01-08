415306c9dbbbac9b92d69e4a962603d8f1e7d741
nicholas.finefter <nfinefter@gmail.com>
2022-01-07 16:46:32 -0800

    Finished

--- MillerRabinPrimalityTest/Program.cs
+++ MillerRabinPrimalityTest/Program.cs
@@ -5,9 +5,21 @@ namespace MillerRabinPrimalityTest
 {
     class Program
     {
+        static void RemoveAll(List<int> values, int n)
+        {
+            for (int i = 0; i < values.Count; i++)
+            {
+                if (n == values[i])
+                {
+                    values.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
         static List<int> FindPrimeFactorization(int num)
         {
-            List<int> allPrimeFactors = new List<int>();
+            List<int> primeFactorization = new List<int>();
 
             for (int i = 2; i <= num; i++)
             {
@@ -15,35 +27,64 @@ namespace MillerRabinPrimalityTest
                 {
                     if (isPrime(i))
                     {
-                        allPrimeFactors.Add(i);
+                        num /= i;
+                        primeFactorization.Add(i);
+                        i--;
                     }
-
-                    
-                    
                 }
-              
             }
 
-            List<int> primeFactorization = new List<int>();
-
-            while (!isPrime(num))
-            { 
-                num = num / allPrimeFactors[0];
-                for (int j = 2; j < num; j++)
+            /*
+            for (int i = 0; i < primeFactorization.Count; i++)
+            {
+                while (!isPrime(primeFactorization[i]))
                 {
-                    if (num % j == 0)
+                    for (int j = 2; j <= num; j++)
                     {
-                        if (isPrime(j))
+                        if (num % j == 0)
                         {
-                            primeFactorization.Add(j);
-
+                            if (isPrime(j))
+                            {
+                                primeFactorization.Add(j);
+                                j--;
+                            }
                         }
                     }
                 }
+            }*/
 
+            /*
+             * Given a number n
+             * if j is a factor, add it to result
+             * factor it out (divide) from n
+             * 
+             * 50 = 2 * 25
+             * add 2 to output
+             * repeat with n / 2
+             * 25 = 5*5
+             * add 5 to output
+             * repeat with n / 5
+             */
+            return primeFactorization;
+        }
+
+        static List<int> PrimeFactorizationRecursive(int num)
+        {
+            if (num == 1 || num == 0)
+            {
+                return new List<int>();
             }
-
-            return allPrimeFactors;
+            for (int i = 2; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    List<int> primeFactorization = PrimeFactorizationRecursive(num / i);
+                    primeFactorization.Insert(0, i);
+                    return primeFactorization;
+                }
+            }
+            //Should not be reached
+            return null;
         }
 
         static bool isPrime(int num)
@@ -61,16 +102,28 @@ namespace MillerRabinPrimalityTest
 
         static void Main(string[] args)
         {
+            /*RecursionIntro recursionIntro = new RecursionIntro();
+            
+            Console.WriteLine("Enter a number:");
+            int userNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(recursionIntro.Factorial(userNum));
+            Console.WriteLine($"The {userNum}th Fibonacci number is {recursionIntro.Fibonacci(userNum)}");
+            recursionIntro.Countdown(userNum);
+            
+            int[] myArray = new int[] { 1, 2, 3, 3 };
+            recursionIntro.ContainsDouble(myArray);
+             */
+
+            //Console.WriteLine("---Actual Primality Stuff---");
+
             Console.WriteLine("Give Number!");
             int num = int.Parse(Console.ReadLine());
-            List<int> primeFactors = FindPrimeFactorization(num);
+            List<int> primeFactors = PrimeFactorizationRecursive(num);
 
             for (int i = 0; i < primeFactors.Count; i++)
             {
- 
                 Console.WriteLine(primeFactors[i]);
             }
-            
         }
     }
 }
---
+++ MillerRabinPrimalityTest/RecursionIntro.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MillerRabinPrimalityTest
+{
+    class RecursionIntro
+    {
+        public int FactorialIterative(int n)
+        {
+            int num = 1;
+            for (int i = 0; i < n; i++)
+            {
+                num *= n - i;
+            }
+            return num;
+        }
+
+        //n! = n * (n - 1) * (n - 2) * ...
+        //0! = 1! = 1
+        //n! = n * (n - 1)!
+        public int Factorial(int n)
+        {
+            if(n == 0 || n == 1)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+
+        //Fibonacci sequence:
+        //1 1 2 3 5 8 13 ...
+        //f(n): the nth fibonacci number
+        //f(1) = 1
+        //f(2) = 1
+        //f(n) = f(n - 1) + f(n - 2)
+
+        public int Fibonacci(int n)
+        {
+            if(n <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(n));
+            }
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        //Countdown(5) should print:
+        //5 4 3 2 1
+        //Liftoff!
+        //note: on separate lines
+        public void Countdown(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentOutOfRangeException("out of range");
+            }
+            if (n == 0)
+            {
+                Console.WriteLine("Liftoff!");
+            }
+            else
+            {
+                if (n == 1)
+                {
+                    Console.WriteLine(n);
+                }
+                else
+                {
+                    Console.Write(n + " ");
+                }
+                Countdown(n - 1);
+            }
+        }
+
+        //Return true if the array contains the same value in adjacent spots, false otherwise
+        //[1, 2, 3, 3]
+        //purpose of start: don't consider indices less than start
+        public bool ContainsDouble(int[] values, int start = 0)
+        {
+            if (start + 1 >= values.Length)
+            {
+                return false;
+            }
+            if (values[start] == values[start + 1])
+            {
+                return true;
+            }
+            return ContainsDouble(values, start + 1);
+        }
+    }
+}
